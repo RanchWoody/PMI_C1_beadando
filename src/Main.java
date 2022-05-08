963a6b518e0b92d27d1bac5bfa4cfe2dfa4fb0c8
RanchWoody <szb200294@gmail.com>
2022-05-08 17:58:04 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,13 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.util.Scanner;
 
-
-
-
-
-
 public class Main {
 
     public static void main(String[] args) {
 
+        /*A program órák nyilvántartására szolgál. Az órák taralmaznak egy tantárgyi megnevezést, a tanító tanár nevét, egy számot mely
+        meghatározza, hány darab van belőle egy héten és hogy kötelező e.
+         */
         menu();
 
     }
@@ -99,6 +97,7 @@ public class Main {
                         System.out.println("Kérem adja meg hetente hány ilyen óra van: ");
                         hetiDB.appendChild(doc.createTextNode(String.valueOf(be.nextInt())));
 
+
                         //mivel egy boolean adat így egy switch case-el csak az igen vagy nem opciót lehet választani
                         System.out.println("Kötelező az óra?\n1 - Igen\n2 - Nem");
                         boolean vege = true;
@@ -128,41 +127,39 @@ public class Main {
                         root.appendChild(ujOra);
 
                         //az xml elmentése illetve a formázása
-                        Transformer transformer = TransformerFactory.newInstance().newTransformer();
-                        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-                        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-                        Result output = new StreamResult(new File("orak.xml"));
-                        Source input = new DOMSource(root);
-
-                        transformer.transform(input, output);
+                        transformer(doc);
                         break;
                     case 3:
 
                         be.nextLine();
 
+                        //a szerkesztendő óra adatainak bekérése
                         System.out.println("Kérem adja meg a szerkesztendő óra nevét: ");
                         String szerkNev = be.nextLine();
                         System.out.println("Illetve a tanárát: ");
                         String szertTan = be.nextLine();
                         boolean voltE = true;
 
+                        //for ciklussal végigmegyünk az órákon
                         for (int i = 1; i < doc.getDocumentElement().getChildNodes().getLength(); i+=2)
                         {
-                            NodeList asd = doc.getDocumentElement().getChildNodes().item(i).getChildNodes();
+                            NodeList nodeList = doc.getDocumentElement().getChildNodes().item(i).getChildNodes();
 
-                            if(asd.item(1).getTextContent().equals(szerkNev) && asd.item(3).getTextContent().equals(szertTan))
+                            //leellenrőizzük hogy egyeznek e a megadott adatokkal, ha igen módosítjuk őket külön bekérésekkel
+                            if(nodeList.item(1).getTextContent().equals(szerkNev) && nodeList.item(3).getTextContent().equals(szertTan))
                             {
                                 voltE = false;
 
                                 System.out.println("Az új név: ");
-                                asd.item(1).setTextContent(be.nextLine());
+                                nodeList.item(1).setTextContent(be.nextLine());
 
                                 System.out.println("Az új tanár: ");
-                                asd.item(3).setTextContent(be.nextLine());
+                                nodeList.item(3).setTextContent(be.nextLine());
 
                                 System.out.println("Az új heti darabszám: ");
-                                asd.item(5).setTextContent(String.valueOf(be.nextInt()));
+                                nodeList.item(5).setTextContent(String.valueOf(be.nextInt()));
 
+                                //A bool féle adat switch-case bekérése. Így a hibás adatok ezelése egyszerűen elvégezhető.
                                 System.out.println("Kötelező?\n1 - Igen\n2 - Nem");
                                 vege = true;
                                 while (vege)
@@ -170,11 +167,11 @@ public class Main {
                                     switch (be.nextInt())
                                     {
                                         case 1:
-                                            asd.item(7).setTextContent("Igen");
+                                            nodeList.item(7).setTextContent("Igen");
                                             vege = false;
                                             break;
                                         case 2:
-                                            asd.item(7).setTextContent("Nem");
+                                            nodeList.item(7).setTextContent("Nem");
                                             vege = false;
                                             break;
                                         default:
@@ -190,16 +187,46 @@ public class Main {
                             System.out.println("Nincs ilyen óra!");
                         }
 
-                        Transformer tf = TransformerFactory.newInstance().newTransformer();
-                        tf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-                        tf.setOutputProperty(OutputKeys.INDENT, "yes");
-                        Result out = new StreamResult(new File("orak.xml"));
-                        Source in = new DOMSource(doc);
-
-                        tf.transform(in, out);
-
+                        //xml mentése
+                        transformer(doc);
                         break;
                     case 4:
+                        be.nextLine();
+
+                        //a törlendő adatok bekérése és egy bool hogy vizsgáljuk megtörténik e a törlés
+                        System.out.println("Kérem adja meg a törölni kívánt óra nevét: ");
+                        String torlendoOra = be.nextLine();
+                        System.out.println("És a tanár nevét: ");
+                        String torlendoTanar = be.nextLine();
+                        boolean delete = false;
+
+                        //for ciklussal az összes órán végigmegyünk
+                        for (int i = 1; i < doc.getElementsByTagName("ora").getLength(); i++)
+                        {
+                            Node node = doc.getElementsByTagName("ora").item(i);
+                            Element element = (Element)  doc.getElementsByTagName("ora").item(i);
+                            NodeList nodeList = doc.getDocumentElement().getChildNodes().item(i).getChildNodes();
+
+                            //leellenőrizzük hogy egyeznek e a megadott adatok, ha igen töröljük az órát
+                            if(element.getElementsByTagName("nev").item(0).getTextContent().equals(torlendoOra) && element.getElementsByTagName("tanar").item(0).getTextContent().equals(torlendoTanar))
+                            {
+                                doc.getDocumentElement().removeChild(node);
+                                delete = true;
+                            }
+                        }
+
+                        if(delete)
+                        {
+                            System.out.println("TÖRÖLVE");
+                        }
+                        else
+                        {
+                            System.out.println("Hiba a törléskor, nem található a tárgy!");
+                        }
+
+                        //xml mentése
+                        transformer(doc);
+
                         break;
                     default:
                         System.out.println("\nHibás választás!\n");
@@ -207,10 +234,24 @@ public class Main {
             }
             catch (Exception e)
             {
-                System.out.println(e);
+                System.out.println("A program váratlan hiba miatt leáll!");
+                System.out.println("Hibakód: "+e);
+                break;
             }
 
 
         }
     }
+
+    public static void transformer(Document doc) throws TransformerException {
+        Transformer tf1 = TransformerFactory.newInstance().newTransformer();
+        tf1.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        tf1.setOutputProperty(OutputKeys.INDENT, "yes");
+        Result out = new StreamResult(new File("orak.xml"));
+        Source in = new DOMSource(doc);
+
+        tf1.transform(in, out);
+    }
 }
+
+
